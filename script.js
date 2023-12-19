966b006528497a1ce72d46ed5e4d5a8f3a32223e
Kyle Myers <92892736+kyle-p-myers@users.noreply.github.com>
2023-12-19 19:43:44 +0000

    changes

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
-function fetchMoonDetails() {
-    return fetch('https://api.farmsense.net/v1/moonphases/?d=1350526582')
-      .then(response => response.json())
-      .then(data => {
-        const moonNameElement = document.querySelector('.phase-name');
-        const distanceElement = document.querySelector('.distance');
-        moonNameElement.textContent = `${data[0].Moon[0]}`;
-        distanceElement.textContent = `${data[0].Distance} km`;
-      })
-      .catch(error => console.error('Error fetching moon details', error));
-  }
-  
-  function fetchMoonPhase() {
-    return fetch('https://api.farmsense.net/v1/moonphases/?d=1350526582')
-      .then(response => response.json())
-      .then(data => data[0].Illumination);
-  }
-  
-  function mapToMoonPhase(illumination) {
-    if (illumination >= 0.1 && illumination < 49.9) {
-      return 'waxing-crescent';
-    } else if (illumination == 50) {
-      return 'new-moon';
-    } else if (illumination >= 50.1 && illumination <= 99.9) {
-      return 'waxing-gibbous';
-    } else if (illumination == 100) {
-      return 'full-moon';
-    } else {
-      return 'new-moon';
+// Fetch all planets with the X-Api-Key header and parameters
+$.ajax({
+    method: 'GET',
+    url: 'https://api.api-ninjas.com/v1/planets',
+    headers: {
+        'X-Api-Key': 'KPyAryvwq3xkDoePNEb0Gw==xcnkiw0PmV37pZsS'
+    },
+    
+    contentType: 'application/json',
+    success: function(result) {
+        console.log(result);
+
+        // Check if the result contains an array of planets
+        if (result && Array.isArray(result)) {
+            result.forEach(planet => {
+                const button = $('<button></button>');
+                button.text(planet.name);
+                button.on('click', () => handlePlanetButtonClick(planet.name));
+                planetButtonsContainer.append(button);
+            });
+        } else {
+            console.error('Error: No planets found in the API response.');
+        }
+    },
+    error: function ajaxError(jqXHR) {
+        console.error('Error: ', jqXHR.responseText);
     }
-  }
-  
-  function updateMoonImage(moonPhase) {
-    const moonImage = document.getElementById('moonImage');
-    moonImage.src = `media/${moonPhase}.jpg`;
-  }
-  
-  function displayMoonPhase() {
-    fetchMoonPhase()
-      .then(illumination => {
-        const moonPhase = mapToMoonPhase(illumination);
-        updateMoonImage(moonPhase);
-      })
-      .catch(error => console.error('Error fetching moon phase image', error));
-  }
-  
-  window.onload = function () {
-    const loadingSpinner = document.querySelector('.loading-spinner');
-    const mainContent = document.getElementById('mainContent');
-  
-    // Show loading spinner
-    loadingSpinner.style.display = 'block';
-    mainContent.style.display = 'none';
-  
-    // Simulate a delay (you can replace this with your actual data fetching logic)
-    setTimeout(() => {
-      loadingSpinner.style.display = 'none'; // Hide the loading spinner
-      mainContent.style.display = 'flex'; // Show the main content
-      displayMoonPhase();
-      fetchMoonDetails();
-    }, 2000); // Adjust the delay time as needed
-  };
-  
+});
